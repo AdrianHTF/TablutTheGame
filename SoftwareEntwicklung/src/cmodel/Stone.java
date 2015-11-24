7591ef8add14fbf22d3eecca295a19567e073e96
AdrianFien <adfien@hwg-konstanz.de>
2015-11-24 22:58:46 +0100

    renamed

--- SoftwareEntwicklung/src/cmodel/Stone.java
+++ SoftwareEntwicklung/src/cmodel/Stone.java
@@ -6,9 +6,9 @@ package cmodel;
  */
 public class Stone {
 	private int unitSpecification = 0;
-	private static final int attack = 1;
-	private static final int defense = 2;
-	private static final int king = 3;
+	private static final int ATTACK = 1;
+	private static final int DEFENSE = 2;
+	private static final int KING = 3;
 	/**
 	 * 
 	 * @param pUnitSpecification 
@@ -19,13 +19,13 @@ public class Stone {
 		if(pUnitSpecification == 0)
 			unitSpecification = 0;
 		else if(pUnitSpecification == 1){
-			unitSpecification = attack;
+			unitSpecification = ATTACK;
 			}
 		else if(pUnitSpecification == 2){
-			unitSpecification = defense;
+			unitSpecification = DEFENSE;
 		}
 		else if(pUnitSpecification == 3){
-			unitSpecification = king;
+			unitSpecification = KING;
 		}
 		else
 			System.err.println("Unit must be 1, 2 or 3");
